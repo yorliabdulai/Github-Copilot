5ec44fcd2277619813cb9c513bba9d784cb8c1ff
Abdulai Yorli Iddrisu <yorliabdulai1@gmail.com>
2024-06-08 08:04:22 +0000

    Update comments.js

--- comments.js
+++ comments.js
@@ -33,5 +33,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000, () => {
-  console.log('Server is running at http://localhost:3000');
+  console.log('Server is running at http://localhost:3000', Create web server);
 });
