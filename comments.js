ddf5df841c4eb73ce17a0e15b8d66aa8be5d293d
Abdulai Yorli Iddrisu <yorliabdulai1@gmail.com>
2024-06-08 07:48:32 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,37 @@
-// create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-// create a route
-app.get('/comments', (req, res) => {
-  res.send('Comments route');
+// Create a web server
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const querystring = require('querystring');
+const path = require('path');
+
+const comments = [];
+const server = http.createServer((req, res) => {
+  // Get the path
+  const pathObj = url.parse(req.url, true);
+  const pathname = pathObj.pathname;
+  if (pathname === '/') {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end('500 - Internal Server Error');
+      } else {
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data);
+      }
+    });
+  } else if (pathname === '/comment') {
+    const comment = querystring.parse(pathObj.query).comment;
+    comments.push(comment);
+    res.end('success');
+  } else if (pathname === '/getComments') {
+    res.end(JSON.stringify(comments));
+  } else {
+    res.statusCode = 404;
+    res.end('404 - Not Found');
+  }
 });
-// run the server
-app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
+
+server.listen(3000, () => {
+  console.log('Server is running at http://localhost:3000');
 });
